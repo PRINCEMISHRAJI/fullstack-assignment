8f25fb213cda9142c50c6850cddbfc7e880a76fa
Ankit Mishra <50262987+PRINCEMISHRAJI@users.noreply.github.com>
2024-08-21 22:53:04 +0530

    Update app.js
    removed cors error

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,19 @@ import mongoose from "mongoose";
 import helpCardsRoutes from "./routes/helpCards.js"
 import bodyParser from "body-parser";
 import express from "express"
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+const allowedOrigins = ['https://help-center-102o.onrender.com'];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
 
 
 app.use(bodyParser.json());
