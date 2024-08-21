b469ffe72e63eab2d6c3cc95f4c37676add2176c
Ankit Mishra <50262987+PRINCEMISHRAJI@users.noreply.github.com>
2024-08-21 21:33:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/cards': {
-        target: 'http://localhost:3000',
+        target: 'https://fullstack-assignment-7599.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/cards/, '/cards'),
       },
